1d56856080965d9677a446d0bc6991c64b47b808
Aiden York <alyork@crimson.ua.edu>
2024-11-24 16:52:37 -0600

    added function labels

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-const makeURL = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json';
 
 function App() {
+
+    /*
+    These are the react useStates that I created for this coding project
+    The first 3 are used in the useEffects that I created the years in as
+    well as fetched the make and model info from using an api fetch. The 
+    second 3 are used in the return portion of the file in order to set
+    the selected portion of the dropdowns.
+    */
+
     const [year, setYears] = useState([]);
     const [make, setMake] = useState([]);
     const [model, setModel] = useState([]);
@@ -11,65 +19,93 @@ function App() {
     const [selectedMake, setSelectedMake] = useState('');
     const [selectedModel, setSelectedModel] = useState('');
 
+
+    /*
+    This first useEffect was used to generate the years for the first dropdown
+    out of the 3. I used a simple while loop that ended when the year hit 2025
+    */
+
     useEffect(() => {
         let yearCount = 1995;
         let allYears = [];
 
         while (yearCount <= 2025) {
-            allYears.push(yearCount);
-            yearCount++;
+             allYears.push(yearCount);
+             yearCount++;
         }
         setYears(allYears);
     }, []);
 
+
+    /*
+    This second useEffect was to fetch the make info from the api. It is only fetched 
+    after the year is selected
+    */
+
     useEffect(() => {
         async function getMake() {
-            const response = await fetch(makeURL);
-            const data = await response.json();
-            setMake(data.Results);
+             const makeURL = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json';
+             const response = await fetch(makeURL);
+             const data = await response.json();
+             setMake(data.Results);
         }
         if (selectedYear) {
-            getMake();
+             getMake();
         }
     }, [selectedYear]);
 
+    /*
+    This is the final useEffect that I used to get the model info. The model
+    info is only displayed after the make is selected
+    */
+
     useEffect(() => {
         async function getModel() {
-            const modelURL = `https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${selectedMake}?format=json`;
-            const response = await fetch(modelURL);
-            const data = await response.json();
-            setModel(data.Results);
+             const modelURL = `https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${selectedMake}?format=json`;
+             const response = await fetch(modelURL);
+             const data = await response.json();
+             setModel(data.Results);
         }
-        if (selectedMake) {
-            getModel();
+        if (selectedMake && selectedYear) {
+             getModel();
         }
     }, [selectedMake]);
 
+
+    /*
+    This return is used to display the dropdown. It has the text on the left, it
+    then goes into 3 categories: year, make, model. Each of the 3 dropdowns uses a
+    select function. Year is selected first, then make can be selected, then model. 
+    The first option, instead of being blank, is the category name. The rest of the
+    options are generated from the api, and if the option is changed, the new 
+    selection is displayed.
+    */
+
     return (
         <div className="container">
-            <div className="text-container">
-                <h1>SET YOUR VEHICLE</h1>
-                <h2>Get an exact fit for your vehicle</h2>
-            </div>
+          <div className="text-container">
+             <h1>SET YOUR VEHICLE</h1>
+             <h2>Get an exact fit for your vehicle</h2>
+           </div>
 
-            <div className="dropdown-container">
-                <div className="dropdown-row">
-                    <select
+           <div className="dropdown-container">
+              <div className="dropdown-row">
+                  <select
                         id="year-select"
                         value={selectedYear}
                         onChange={(changeYear) => setSelectedYear(changeYear.target.value)}
-                    >
-                        <option value="">1 | Year</option>
-                        {year.map((y) => (
-                            <option key={y} value={y}>
+                   >
+                       <option value="">1 | Year</option>
+                       {year.map((y) => (
+                           <option key={y} value={y}>
                                 {y}
-                            </option>
+                           </option>
                         ))}
-                    </select>
+                  </select>
                 </div>
 
                 <div className="dropdown-row">
-                    <select
+                   <select
                         id="make-select"
                         value={selectedMake}
                         onChange={(changeMake) => setSelectedMake(changeMake.target.value)}
@@ -81,7 +117,7 @@ function App() {
                                 {mk.MakeName}
                             </option>
                         ))}
-                    </select>
+                   </select>
                 </div>
 
                 <div className="dropdown-row">
